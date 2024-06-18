bb1262d3785a841c46e693d6fefa9223e1d73414
luiz-decio <luizgdecio@gmail.com>
2024-06-18 16:13:59 -0300

    update extract and load script

--- src/extract_load.py
+++ src/extract_load.py
@@ -8,19 +8,37 @@ load_dotenv()
 
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
-def get_commodities_data(ticker, period='5d', interval='1d'):
-    ticker = yf.Ticker(ticker)
+# Gett the environment variables
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Create database url and create engine
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+
+def get_commodities_data(symbol, period='5d', interval='1d'):
+    ticker = yf.Ticker(symbol)
     data = ticker.history(period=period, interval=interval)[['Close']]
-    data['simbolo'] = ticker  # Add ticket column
+    data['symbol'] = symbol  # Add ticket symbol column
     return data
 
 def get_all_commodities_data(commodities):
     full_data = []
-    for simbolo in commodities:
-        dados = get_commodities_data(simbolo)
-        full_data.append(dados)
+    for ticker in commodities:
+        data = get_commodities_data(ticker)
+        full_data.append(data)
     return pd.concat(full_data)
+
+def write_to_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+    
 
 if __name__ == "__main__":
     concat_data = get_all_commodities_data(commodities)
-    print(concat_data)
+    write_to_postgres(concat_data, schema='public')
