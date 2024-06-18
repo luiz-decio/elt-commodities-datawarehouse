c82bd41619f18e7ea4cafece76e6264f47399380
luiz-decio <luizgdecio@gmail.com>
2024-06-18 20:19:20 -0300

    add sample data generator

---
+++ src/data_generator.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import random
+import dateutil.relativedelta
+from datetime import datetime
+
+"""
+Script to generate sample commodities sales data
+"""
+
+comodities_list = ["CL=F", "GC=F", "SI=F"]
+ref_day = (datetime.now().date())
+df_generated_data = None
+
+for n in range(10):
+
+    data_day = ref_day - dateutil.relativedelta.relativedelta(days = n)
+    row_n = random.randint(4, 8)
+
+    for _ in range(row_n):
+        data = {
+            'date' : data_day,
+            'symbol' : random.choice(comodities_list),
+            'action' : random.choice(['sell', 'buy']),
+            'quantity' : random.randint(1, 200)
+        }
+        df = pd.DataFrame(data=data, index=[0])
+
+        if df_generated_data is None:
+            df_generated_data = df 
+        else: 
+            df_generated_data = pd.concat([df_generated_data, df])
+
+df_generated_data.to_csv("dw_commodities/seeds/comodities_sell.csv", index=False)
